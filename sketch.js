2dd48c5bb816e7bfd6dc24a83032c3229da75ec6
Akshay04062006 <91886120+Akshay04062006@users.noreply.github.com>
2021-10-11 17:23:27 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,22 +1,43 @@
-var box;
+
 function setup() {
   createCanvas(400,400);
-  box = createSprite(200,200,50,50)
+  background(51);
+  box = createSprite(200,200,30,30);
+
 }
 
 function draw() 
 {
-  background(30);
-  if(keyIsDown(RIGHT_ARROW)){
-    box.x = box.x+5
+
+  // write the code to change background color 
+  // to red when RIGHT_ARROW is pressed
+   
+  if(keyIsDown(RIGHT_ARROW))
+  {
+   
+    background("red");
   }
-  if(keyIsDown(LEFT_ARROW)){
-    box.x = box.x-5
+  
+
+  if (keyIsDown(LEFT_ARROW)) 
+  {
+    background("blue");
+    
+  }
+ 
+    if (keyIsDown(UP_ARROW)) 
+  {
+    background("yellow");
+   
   }
 
+  if (keyIsDown(DOWN_ARROW)) 
+  {
+    background("green");
+  }
+
+
+  
   drawSprites();
 }
-
-
-
 
